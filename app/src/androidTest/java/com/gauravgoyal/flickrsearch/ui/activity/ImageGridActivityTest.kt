b9c950926aafdc333ea0a7fe9d4fa5efa51a126c
Gaurav <gaurav.goyal@swoo.tv>
2019-01-21 11:34:34 +0530

    Added Unit test cases

---
+++ app/src/androidTest/java/com/gauravgoyal/flickrsearch/ui/activity/ImageGridActivityTest.kt
@@ -0,0 +1,103 @@
+package com.gauravgoyal.flickrsearch.ui.activity
+
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.View
+import android.view.ViewGroup
+import com.gauravgoyal.flickrsearch.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class ImageGridActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(ImageGridActivity::class.java)
+
+    @Test
+    fun imageGridActivityTest() {
+        val appCompatImageView = onView(
+            allOf(
+                withId(R.id.search_button), withContentDescription("Search"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.search_bar),
+                        childAtPosition(
+                            withId(R.id.search),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageView.perform(click())
+
+        val searchAutoComplete = onView(
+            allOf(
+                withId(R.id.search_src_text),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.search_plate),
+                        childAtPosition(
+                            withId(R.id.search_edit_frame),
+                            1
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        searchAutoComplete.perform(replaceText("xuz"), closeSoftKeyboard())
+
+        val searchAutoComplete2 = onView(
+            allOf(
+                withId(R.id.search_src_text), withText("xuz"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.search_plate),
+                        childAtPosition(
+                            withId(R.id.search_edit_frame),
+                            1
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        searchAutoComplete2.perform(pressImeActionButton())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
---
+++ app/src/test/java/com/gauravgoyal/flickrsearch/PresenterTestCase.java
@@ -0,0 +1,69 @@
+package com.gauravgoyal.flickrsearch;
+
+import com.gauravgoyal.flickrsearch.contract.ListContract;
+import com.gauravgoyal.flickrsearch.contract.ListPresenter;
+import com.gauravgoyal.flickrsearch.model.PhotoModel;
+import com.gauravgoyal.flickrsearch.model.PhotoServiceResponse;
+import com.gauravgoyal.flickrsearch.repo.PhotosRepo;
+import com.gauravgoyal.flickrsearch.ui.fragments.BaseFragment;
+import com.gauravgoyal.flickrsearch.util.utility.ExtKt;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+
+public class PresenterTestCase {
+
+
+    private ListPresenter presenter;
+
+    @Mock
+    private ListPresenter mockedPresenter;
+
+    @Mock
+    private BaseFragment baseFragment;
+
+    private PhotosRepo photosRepo;
+
+    @Mock
+    private ListContract.View view;
+
+    @Mock
+    private PhotosRepo.OnLoadListener loadListener;
+
+    private PhotoServiceResponse response;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        presenter = new ListPresenter(view);
+        photosRepo = new PhotosRepo("kitten",1, loadListener);
+        response = new PhotoServiceResponse(new ArrayList<PhotoModel>(),1,10,100);
+    }
+
+
+
+    @Test
+    public void fetchValidDataShouldLoadIntoView() {
+        presenter.onLoadComplete(response);
+        Mockito.verify(view, Mockito.times(1)).showList(response);
+    }
+
+    @Test
+    public void fetchValidDataShouldLoadIntoErrorView() {
+        presenter.onError();
+        Mockito.verify(view, Mockito.times(1)).showErrorBox();
+    }
+
+    @Test
+    public void testImageUrl() {
+        PhotoModel photoModel = new PhotoModel("secret",1,"id", "server");
+        String url = ExtKt.getImageUrl(photoModel);
+        Assert.assertEquals(url,"http://farm1.static.flickr.com/server/id_secret.jpg");
+    }
+
+}
